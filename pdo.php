033a27bbba8ad1cdd9a4886be73a50a1299aa046
Lashen Kariyawasam <56374597+LashenKariyawasam@users.noreply.github.com>
2021-12-11 12:38:42 +0530

    grant persmissions

--- pdo.php
+++ pdo.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $pdo = new PDO('mysql:host = localhost;port=3306;dbname=misc', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc', 'fred', 'zap');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e){
     echo "Connection failed : ". $e->getMessage();
